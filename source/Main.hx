33a2f2f83aaa8a46aca4984a91e041fdc088de5f
UlizarButNomore <116612566+UlizarButNomore@users.noreply.github.com>
2022-10-27 17:11:06 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -71,6 +71,7 @@ class Main extends Sprite
 		#end
 
 		Paths.getModFolders();
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 		il.check();
 
 		#if !mobile
