828648e06a354c99d549b2e16130f66bf2aa80f2
UlizarButNomore <116612566+UlizarButNomore@users.noreply.github.com>
2022-10-27 16:53:13 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -29,6 +29,7 @@ class Main extends Sprite
 
 	public function new()
 	{
+		SUtil.uncaughtErrorHandler();
 		super();
 
 		if (stage != null)
@@ -70,7 +71,7 @@ class Main extends Sprite
 		#end
 
 		Paths.getModFolders();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		il.check();
 
 		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
